b20968feb3eebc73bd12cce3a091fcfc1e8e0151
dougfunny1983 <douglaseduardo220483@gmail.com>
2020-01-20 19:36:08 -0300

    Colocando a api para funcionar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './trivia.png';
 import './App.css';
+import getTokenTriviaAPI  from './service/APIService';
 
 export default function App() {
+  getTokenTriviaAPI()
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          SUA VEZ
+          ijbuhbghgb
         </h1>
       </header>
     </div>
