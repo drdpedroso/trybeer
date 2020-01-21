d361a998c397ac254fddc0a61d1bd07eb995ba44
dougfunny1983 <douglaseduardo220483@gmail.com>
2020-01-21 18:18:29 -0300

    Terminando o componente Configuranções

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
-import logo from './trivia.png';
-import './App.css';
-import getTokenTriviaAPI from './service/APIService';
-import Configurations from './Configurations';
-=======
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // import logo from './trivia.png';
 // import './App.css';
 import Home from './components/Home';
->>>>>>> master
+import Configurations from "./components/Configurations";
 
 export default function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Configurations />
-      </header>
-    </div>
-=======
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
@@ -30,6 +16,5 @@ export default function App() {
         {/* <Route></Route> ranking */}
       </Switch>
     </BrowserRouter>
->>>>>>> master
   );
 }
---
+++ src/components/Configurations.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import '../css/Configurations.css';
+
+class Configurations extends Component {
+  creatSelect = (nameLabel, subs1, subs2, subs3) => (
+    <label>
+      {nameLabel}
+      <select name={nameLabel}>
+        <option value={`${nameLabel}-1`}>{subs1}</option>
+        <option value={`${nameLabel}-1`} selected>
+          {subs2}
+        </option>
+        <option value={`${nameLabel}-1`}>{subs3}</option>
+      </select>
+    </label>
+  );
+  render() {
+    return (
+      <div className="container">
+        {/* {this.creatSelect('Tipo', 'valor-1', 'valor-2', 'valor-3')} */}
+        <h2>Configurações</h2>
+        {this.creatSelect('Categoria', 'Games', 'Filmes', 'Séries')}
+        {this.creatSelect('Dificuldade', 'Moleza', 'EitaPorra', 'Fudeu')}
+        {this.creatSelect('Tipo', 'valor-1', 'valor-2', 'valor-3')}
+      </div>
+    );
+  }
+}
+
+export default Configurations;
