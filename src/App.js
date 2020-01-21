b3662a7b558d270a0ddd1d3c095fd5483db5c6c2
dougfunny1983 <douglaseduardo220483@gmail.com>
2020-01-21 14:28:56 -0300

    cc

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import logo from './trivia.png';
 import './App.css';
-import getTokenTriviaAPI  from './service/APIService';
+import getTokenTriviaAPI from './service/APIService';
 
 export default function App() {
-  getTokenTriviaAPI()
+  getTokenTriviaAPI();
 
-  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          ijbuhbghgb
-        </h1>
+        <h1>ijbuhbghgb</h1>
       </header>
     </div>
   );
