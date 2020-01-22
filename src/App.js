8e41ad151398d9dba6cbc6b7e50822d1e52dbe81
Eduardo <drdpedroso@gmail.com>
2020-01-21 22:31:37 -0300

    Start coding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import axios from 'axios'
+import {useHistory} from 'react-router-dom'
 // import logo from './trivia.png';
 // import './App.css';
-import Home from './components/Home';
-// import Configurations from "./components/Configurations";
+
+
+const Home = () => {
+    const [cards, setCards] = React.useState([])
+    const history = useHistory()
+    useEffect(() => {
+        // [1,2,3,4,5].forEach((i) => {
+            axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
+                .then(function (response) {
+                    setCards(cards.concat(response.data.meals[0]))
+                })
+        // })
+
+    }, [])
+        console.log(cards)
+    return <div>
+        {cards.map((r) => {
+            return <div onClick={() => history.push(`/receita/${r.idMeal}`)}>
+                <img src={r.strMealThumb} style={{width: 200, heigh: 200}}/>
+                <p>{r.strCategory}</p>
+                <p>{r.strMeal}</p>
+            </div>
+        })}
+    </div>
+}
 
 export default function App() {
+    const [state, setState] = React.useState({})
   return (
     <BrowserRouter>
       <Switch>
